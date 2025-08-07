a237620c6619969954f121183df542414ee8c928
Praveen N <31819181+praveenncse@users.noreply.github.com>
2025-08-07 07:36:31 +0530

    Time: 1119 ms (47.37%) | Memory: 98.5 MB (38.60%) - LeetSync

---
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxCollectedFruits(self, fruits: List[List[int]]) -> int:
+        @lru_cache(9000)
+        def dp(row: int, col: int)-> int:
+
+            if 0 <= col < row < n:                                  
+                return fruits[row][col] + max(dp(row-1, col+1),
+                                              dp(row  , col+1),
+                                              dp(row+1, col+1))
+            return 0
+
+
+        n = len(fruits)
+        diag = sum(map(lambda x:fruits[x][x], range(n)))   
+
+        upper = dp(n - 1, 0)                               
+
+        dp.cache_clear()                                    
+        fruits = list(zip(*fruits))                         
+        lower = dp(n - 1, 0)                               
+
+        return diag + upper + lower
